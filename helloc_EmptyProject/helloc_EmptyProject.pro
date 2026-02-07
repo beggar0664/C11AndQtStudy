f7595dcbb19104c0c9f310ad224a46fe62331b74
雷金城 <952813664@qq.com>
2026-02-07 14:18:34 +0800

    add [Qt Designer Form]

--- helloc_EmptyProject/helloc_EmptyProject.pro
+++ helloc_EmptyProject/helloc_EmptyProject.pro
@@ -3,3 +3,6 @@ greaterThan(QT_MAJOR_VERSION, 4):QT+=widgets
 SOURCES += \
     main.cpp
 
+FORMS += \
+    hello.ui
+
--- helloc_EmptyProject/main.cpp
+++ helloc_EmptyProject/main.cpp
@@ -1,17 +1,29 @@
+#if 0
+/*
+ * 使用"< >" 系统会到默认目录--安装的include 目录
+ * (D:\qt5_14_2\ide\5.14.2\msvc2017\include)查找包含的头文件
+*/
 #include<QApplication>
 #include<QDialog>
 #include<QLabel>
-
+#else
+#include"ui_hello.h"
+#endif
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);//QApplication 类对象 用于管理 应用程序的资源，任何一个QT widgets程序都需要一个 QApplication对象
     QDialog w;//对话框
+#if 0
     /* 对话框太小*/
     w.resize(400,300);
     QLabel label(&w);// 对话框是它的父窗口，标签 放在对话框 窗口里
     label.move(200,150);//标签位置
 //    label.setText("hello");//显示的内容
     label.setText( QObject::tr("hello你好啊") );//显示的内容 QObject::tr()函数实现多语言支持
+#else
+    Ui::helloDialog ui;
+    ui.setupUi(&w);//生成界面
+#endif
     w.show();//显示对话框
     return app.exec();//进入事件循环，运行时接收产生的 事件
 }
